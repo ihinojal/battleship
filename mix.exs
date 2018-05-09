5a00eb4a5a76d8918c5b5bb08ebaf1c53f33a36d
Ivan H <ivan@drails.com>
2018-05-09 19:08:46 +0200

    Enable documentation generation using ExDoc
    Add project documentation suggested by ExDoc in `mix.ex` and ExDoc library.

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Battleship.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/ihinojal/btctool"
+
   def project do
     [
       app: :battleship,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Docs
+      source_url: @github_url,
+      #homepage_url: "https://battleship.pw??",
+      docs: docs()
     ]
   end
 
@@ -23,8 +29,18 @@ defmodule Battleship.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Battleship", # The main page in the docs
+      # source_ref: "v#{@version}",
+      # logo: "path/to/logo.png",
+      extras: ["README.md"]
     ]
   end
 end
